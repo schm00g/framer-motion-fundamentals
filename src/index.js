88871f1fbb7d7d1e86b343b9970baf333b265c41
schm00g <hk8igureu@relay.firefox.com>
2022-02-10 17:25:12 +0000

    routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import { render } from "react-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Animation from "./routes/animation";
+import Keyframes from "./routes/keyframes";
+import Transition from "./routes/transition";
+import Gestures from "./routes/gestures";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="animation" element={<Animation />} />
+      <Route path="keyframes" element={<Keyframes />} />
+      <Route path="transition" element={<Transition />} />
+      <Route path="gestures" element={<Gestures />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
@@ -15,3 +30,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// how to use react-router
+// https://github.com/remix-run/react-router/blob/main/docs/getting-started/tutorial.md
+
+// TODO: had to roll back to framer-motion 5 - y tho?
